f6fcc19305fdeca9713276b29eb36c0c4d52f30a
manodya <manodyaabeysinghe7@gmail.com>
2019-11-28 13:47:20 +0530

    add all files

---
+++ RegistrationWithImage/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using RegistrationWithImage.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RegistrationWithImage.Controllers
+{
+    public class ProductController : Controller
+    {
+        OnlineShopEntities db = new OnlineShopEntities();
+        public ActionResult AddNewProduct()
+        {
+            return View();
+        }
+
+        public ActionResult SaveData(tblProduct item)
+        {
+            return View();
+        }
+    }
+}
